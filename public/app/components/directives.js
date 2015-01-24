d974b164c463413f5fdcb7aba7ff03b608fc83f6
hyber1z0r <jakobsky@live.dk>
2015-01-25 00:28:05 +0100

    Changed photogallery to directive

--- public/app/components/directives.js
+++ public/app/components/directives.js
@@ -3,13 +3,10 @@
 /* Directives */
 
 angular.module('hyber.directives', []).
-  directive('angularLinks', function () {
+  directive('photoGallery', function () {
     return {
       restrict: 'AE',
       replace: 'true',
-      template:  '<ul style="list-style-type: none">' +
-        '<li><a href="http://www.sitepoint.com/practical-guide-angularjs-directives/">A practical Guide</a></li>'+
-        '<li><a href="http://weblogs.asp.net/dwahlin/creating-custom-angularjs-directives-part-i-the-fundamentals">Creating Custom Directives</a></li>'+
-      '</ul>'
+      templateUrl: '/app/components/tpl/photogal.html'
     };
   });
