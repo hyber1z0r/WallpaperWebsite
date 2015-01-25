298cbced87897ffb8697c25ff09bd0adb0962697
hyber1z0r <jakobsky@live.dk>
2015-01-25 11:25:16 +0100

    Added test coverage with istanbul

--- public/app/components/directives.js
+++ public/app/components/directives.js
@@ -3,15 +3,15 @@
 /* Directives */
 
 angular.module('hyber.directives', []).
-  directive('photoGallery', function () {
-    return {
-      restrict: 'AE',
-      replace: 'true',
-		link: function (scope, element, attributes) {
-			scope.overskrift = attributes.overskrift;
-			scope.beskrivelse = attributes.beskrivelse;
-			scope.fa = attributes.fa;
-		},
-      templateUrl: '/app/components/tpl/photogal.tpl.html'
-    };
-  });
+	directive('photoGallery', function () {
+		return {
+			restrict: 'AE',
+			replace: 'true',
+			link: function (scope, element, attributes) {
+				scope.overskrift = attributes.overskrift;
+				scope.beskrivelse = attributes.beskrivelse;
+				scope.fa = attributes.fa;
+			},
+			templateUrl: '/app/components/tpl/photogal.tpl.html'
+		};
+	});
